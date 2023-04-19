f779fa221524f82176c713efd1aae163d8b0e968
Jonathan Yau <jonathan_yau@outlook.com>
2023-04-19 18:00:56 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Spotity Playlist Creator
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header(props) {
+    return(
+        <div>
+            <img />
+            <h1 className='Header-title' alt='title'>Jamming</h1>
+        </div>
+    );
+};
+
+
+export default Header;
